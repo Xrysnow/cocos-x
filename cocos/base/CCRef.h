a1608a204adae63a55b607cc33c19c2d37bfe706
Xrysnow <xrysnow@outlook.com>
2024-03-31 12:42:01 +0800

    improve memory layout of Ref

--- cocos/base/CCRef.h
+++ cocos/base/CCRef.h
@@ -144,13 +144,13 @@ public:
     unsigned int _ID;
     /// Lua reference id
     int _luaID;
-    /// scriptObject, support for swift
-    void* _scriptObject;
-
     /**
      When true, it means that the object was already rooted.
      */
     bool _rooted;
+    /// scriptObject, support for swift
+    void* _scriptObject;
+
 #endif
 
     // Memory leak diagnostic data (only included when CC_REF_LEAK_DETECTION is defined and its value isn't zero)
