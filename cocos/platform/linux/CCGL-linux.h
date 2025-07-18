8132dad1b63389ac330d1dff1b5b2ed8705b2a5d
Xrysnow <xrysnow@outlook.com>
2025-07-18 23:33:38 +0800

    fix header

--- cocos/platform/linux/CCGL-linux.h
+++ cocos/platform/linux/CCGL-linux.h
@@ -23,5 +23,3 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
 #pragma once
-
-#include "glad/gl.h"
